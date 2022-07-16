042cc8c752635de6b0b903208c176aeb330e40d0
Renat <its.renatt@gmail.com>
2022-07-16 22:07:45 +0300

    second task done

--- 2/2.py
+++ 2/2.py
@@ -1,14 +1,25 @@
 from mwclient import Site
 import animation
+import string
+
+alphabet = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ' + string.ascii_uppercase 
+animals = dict.fromkeys(alphabet, 0) 
 
 @animation.simple_wait
 def parse():
         site = Site('ru.wikipedia.org')
-        category = site.categories[r'Животные_по_алфавиту']
+        category = site.categories['Животные_по_алфавиту']
         with open('animals.txt', 'w') as f:
                 for page in category:
-                        f.write(page.name)
-                        f.write('\n')
-
+                        name = page.name
+                        if name != 'Категория:Знаменитые животные по алфавиту' and name != 'Категория:Породы собак по алфавиту':
+                                animals[name[0]] += 1
+                                f.write(name)
+                                f.write('\n')
+        for i in animals:
+                print(i, animals[i], sep=':')
 if __name__ == '__main__':
-    parse()
+        try:
+                parse()
+        except Exception as e:
+                print(f'Exception was caught: {e.__class__.__name__}')
